df397c861a4bd39c95901c60440152b94830e400
Weston Chambers <wchambers@captechconsulting.com>
2019-06-15 09:14:40 -0400

    Initial Commit

---
+++ ReadTheRoom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadTheRoom
+//
+//  Created by Weston Chambers on 6/15/19.
+//  Copyright © 2019 Weston Chambers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
